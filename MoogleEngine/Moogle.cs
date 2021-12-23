9e0e2a3910840379e87b99144c6100c9d8f28a64
theGitNoob <racosta011220@gmail.com>
2021-12-23 15:31:25 -0500

    Added method for file indexing

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -1,15 +1,64 @@
-﻿namespace MoogleEngine;
-
-
+﻿using System.Text;
+using System.Collections.Generic;
+namespace MoogleEngine;
 public static class Moogle
 {
-    public static SearchResult Query(string query) {
+
+    static Dictionary<String, int> wordList = new Dictionary<String, int>();
+
+    public static void StartIndex()
+    {
+        Console.WriteLine(Environment.GetEnvironmentVariables()["CONTENT_PATH"]);
+
+        var files = Directory.EnumerateFiles("../Content", "*.txt");
+
+        foreach (string file in files)
+        {
+            ReadFile(file);
+        }
+
+        Console.WriteLine(wordList.Count);
+
+    }
+
+
+    private static void ReadFile(string filename)
+    {
+        StreamReader reader = new StreamReader(filename);
+
+        StreamWriter writer = new StreamWriter("pepe.txt");
+
+        String fullText = reader.ReadToEnd();
+
+        String[] words = fullText.Split(new char[] { ' ', ',', '.', ';' });
+
+        foreach (string word in words)
+        {
+            String lowerWord = word.ToLower();
+            if (wordList.ContainsKey(lowerWord))
+            {
+                wordList[lowerWord]++;
+            }
+            else
+            {
+                wordList.Add(lowerWord, 1);
+            }
+        }
+
+        // foreach (KeyValuePair<string, int> pair in wordList)
+        // {
+        //     Console.WriteLine("The word: {0} appears {1} times on the collection", pair.Key, wordList[pair.Key]);
+        // }
+
+    }
+    public static SearchResult Query(string query)
+    {
         // Modifique este método para responder a la búsqueda
 
         SearchItem[] items = new SearchItem[3] {
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+            new SearchItem("Rae.txt", "Rae score is 21.2", 21.2f),
+            new SearchItem("rockyou.txt", "My rockyou.txt score is 0.5", 0.5f),
+            new SearchItem("pepe.txt", "My pepe.txt score is 0.1", 0.1f),
         };
 
         return new SearchResult(items, query);
--- MoogleServer/Program.cs
+++ MoogleServer/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MoogleEngine;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+Moogle.StartIndex();
 
 
 app.UseStaticFiles();
